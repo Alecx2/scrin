4668fc060bbd2dd53a9801a18a23ce5e6164d406
Alecx2 <149591200+Alecx2@users.noreply.github.com>
2023-11-01 17:40:56 +0400

    add scripts

---
+++ destroy.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject);
+        //даный скрипт унечтожает объект
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
